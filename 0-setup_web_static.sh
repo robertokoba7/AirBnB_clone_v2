f67f21c3a33f1a8c2991c482b9888f84688535d0
robertokoba7 <okobarobert7@gmail.com>
2022-12-22 13:22:03 +0300

    setting up the server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
-# sets up server for web static
+#A bash script that sets up my server for the deployment of web static
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo echo -e "<html>
-<head></head>
-<body>
- ALX School
-</body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-file=/etc/nginx/sites-available/default
-phrase="location /hbnb_static/{\n\talias /data/web_static/current/;\n}\n"
-sudo sed -i "27i $phrase" $file
+#sudo apt-get upadate
+#sudo apt-get install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
+#adding a test string
+echo "<h2> welcome </h2>" > /data/web_static/releases/test/index.html
+echo "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        error_page 404 /404.html;
+        location = /404.html {
+                internal;
+        }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        location /hbnb_static {
+                alias /data/web_static/current/index.html;
+        }
+}" | sudo tee /etc/nginx/sites-available/default
+#symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+#restarting nginx
 sudo service nginx restart
