53d162f913074cd7eaf092b6a1554b1473b5a1b6
robertokoba7 <okobarobert7@gmail.com>
2023-01-14 14:15:10 +0300

    [add] /python/(<text>)

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -17,20 +17,18 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    text = text.replace('_', " ")
-    """Returns C is fun!"""
-    return 'C {}'.format(text)
+def c_text(text):
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """Returns python is magic and also 'is cool' by default"""
-    if text != 'is cool':
-        text = text.replace('_', " ")
-    """Returns python is magic with default text "is cool"!"""
-    return 'python {}'.format(text)
+def python_text(text='is cool'):
+    """ Function called with /python/<text> route """
+    if text is not 'is cool':
+        text = text.replace('_', ' ')
+    return 'Python %s' % text
 
 
 if __name__ == '__main__':
