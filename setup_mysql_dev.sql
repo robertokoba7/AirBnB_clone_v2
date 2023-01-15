343f9e18d6d7cba602b11f7115963220c1cde7a4
robertokoba7 <okobarobert7@gmail.com>
2023-01-15 22:38:02 +0300

    [add] fix to my setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
 -- with the password : hbnb_dev_pwd if it dosen't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Y28240978.h';
 -- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
